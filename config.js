8870acfcb8145fb3341402606d2f50d5ef4c8748
Zinet Hyssen <zinethyssen@users.noreply.github.com>
2025-06-15 15:24:13 -0400

    Fixed config.js to have the correct credentials

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "",
-  database: "tictactoe"
+  user: "436_mysql_user",
+  password: "123pwd456",
+  database: "436db"
 });
 
 db.connect(err => {
