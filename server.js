d828479000e3b1f09f365e2fd6bb3f3a71110307
Zinet Hyssen <zinethyssen@users.noreply.github.com>
2025-06-15 14:17:24 -0400

    Changed port number and updated sql info to reflect class requirements

--- server.js
+++ server.js
@@ -117,4 +117,4 @@ io.on("connection", socket => {
   });
 });
 
-server.listen(3000, () => console.log("Server running on http://localhost:3000"));
+server.listen(8081, () => console.log("Server running on http://localhost:3000"));
